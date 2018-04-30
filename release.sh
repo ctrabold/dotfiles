baabb30dbef9aaf88dbeb4d230d4064a1e40abdb
Christian Trabold <info@christian-trabold.de>
2018-04-30 14:41:13 +0700

    release: Remove unneeded code

--- release.sh
+++ release.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -e
-
-# Release script
-#
 # This script tests the repository with 'bats' test
 # and creates a new stable tag when the tests are green.
 #
@@ -9,12 +6,6 @@
 # before releasing a new 'stable' branch.
 #
 # @see https://github.com/sstephenson/bats
-
-# if ! git diff --exit-code --quiet; then
-#   echo "WARN Please commit or stash all your local changes before releasing."
-#   exit 1
-# fi
-
 echo "INFO installing bats to run tests."
 mkdir -p bin
 cd bin
@@ -27,7 +18,3 @@ echo "INFO installing bats. DONE"
 
 echo "Run tests..."
 ./bin/bats test/*.bat
-
-# echo "Push new 'stable' tag"
-# git tag --force stable
-# git push --force origin refs/tags/stable:refs/tags/stable
