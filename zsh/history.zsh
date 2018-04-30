f2f56f969afce34eab087dcbb065ecee79beeed9
Christian Trabold <info@christian-trabold.de>
2018-04-30 18:51:39 +0700

    zsh/history: Adjust search size
    See https://github.com/zsh-users/zaw#key-binds-and-styles

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -5,6 +5,12 @@ HISTFILE=~/.zsh_history
 
 bindkey '^R' zaw-history
 
+zstyle ':filter-select:highlight' matched fg=green
+zstyle ':filter-select' max-lines 7
+zstyle ':filter-select' case-insensitive yes
+zstyle ':filter-select' hist-find-no-dups yes
+zstyle ':filter-select' extended-search yes
+
 # setopt append_history
 # setopt inc_append_history
 # setopt extended_history
