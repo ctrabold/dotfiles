c415d017c7b2c104c19059b0201e753bf76167c0
Christian Trabold <christian.trabold+codes@gmail.com>
2021-12-30 20:45:15 +0100

    hooks: create `workspace` directory

--- hooks/pre-up/create-dotfiles-local-folder.sh
+++ hooks/pre-up/create-dotfiles-local-folder.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# This script creates a local dotfiles folder
+# This script creates default folders
+# 1) a local dotfiles folder
 #
 # Files in this folder override the 'default' files
 # so you can modify the dotfiles without forking.
 #
 # Use the same filenames to override the default files.
+#
+# 2) a central folder for projects called 'workspace'
 
 mkdir -p $HOME/dotfiles-local
+mkdir -p $HOME/workspace
