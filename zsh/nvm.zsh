03356ac819c98379e600fc260fc1317c5e90b1c1
Christian Trabold <christian.trabold+codes@gmail.com>
2023-04-12 12:05:57 +0200

    zsh/nvm: add aliases
    Not sure if they are acutally needed anymore. See 203e6dd9e

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -2,3 +2,6 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+alias node='unalias node ; unalias npm ; nvm use default ; node $@'
+alias npm='unalias node ; unalias npm ; nvm use default ; npm $@'
